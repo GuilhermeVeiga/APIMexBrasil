d60d570a7bf38a89d83df3e9897fe453d4a1d78e
unknown <guilhermeveiga19@gmail.com>
2019-04-09 13:09:56 -0300

    alteração de string de conexão

--- src/DataBase/db.js
+++ src/DataBase/db.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql'); 
 
 const mysqlConn = mysql.createConnection({
-    host :'localhost',
+    /*host :'localhost',
     user : 'root',
     password : '1234',
-    database : 'API'
-    /*host :'guilhermeeyasmin.com.br',
+    database : 'API' */
+    host :'guilhermeeyasmin.com.br',
     user : 'guilherm_root',
     password : 'yasmin3106',
-    database : 'guilherm_reactDB' */
+    database : 'guilherm_reactDB'
 });
 
 mysqlConn.connect((err) => {
@@ -18,20 +18,9 @@ mysqlConn.connect((err) => {
     console.log('erro ao tentar se conectar \n Error : ' + JSON.stringify(err, undefined, 2));
 });
 
-// mysqlConn.query(sqlQry, function(error, results){
-//     if(error) 
-//       res.json(error);
-//     else
-//       res.json(results);
-//       mysqlConn.end();
-//     console.log('executou!');
-// });
 
 
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//     if (err) throw err;
-//     console.log('The solution is: ', rows[0].solution);
-//   });
+
 
 
 module.exports = mysqlConn;
